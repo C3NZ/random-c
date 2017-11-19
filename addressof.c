9ae6e7720eea9e0341b171e8de4669341dfe47d5
C3NZ <epichdd@gmail.com>
2017-11-19 18:41:03 -0500

    More on memory addresses and values with pointers

---
+++ addressof.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int int_var = 5;
+	int *int_ptr;
+
+	//Put the adress of int_var into int_ptr
+	int_ptr = &int_var;
+
+	//Address that int_ptr stores
+	printf("int_ptr = 0x%08x\n", int_ptr);
+	//Address of int_ptr
+	printf("&int_ptr = 0x%08x\n", &int_ptr);
+	//Value that int_ptr points to
+	printf("*int_ptr = 0x%08x\n", *int_ptr);
+
+	//Output the addresses/values of our variables
+	printf("int_var is located at 0x%08x and contains %d\n", &int_var, int_var);
+	printf("int_ptr is located at 0x%08x, contains 0x%08x, and points to %d\n\n", &int_ptr, int_ptr, *int_ptr);
+}
